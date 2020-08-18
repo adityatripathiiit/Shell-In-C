63ba6bb246a7a987eca602bedd44709b8d671cee
adityatripathiiit <adityatripathiball@gmail.com>
2020-08-18 20:13:59 +0530

    added rm -r

--- shell.c
+++ shell.c
@@ -23,7 +23,7 @@ void getInput();
 void cdFunction();
 void mkdirFunction();
 void exitFunction();
-void rmFunction(); 
+int rmFunction(); 
 void lsFunction(); 
 void printContent();
 void cpFunction();
@@ -71,8 +71,7 @@ int main (){
             for(int i= 1; i<ARGMAX; i++){
                 if( *argval[i] != '\0' )catFunction(argval[i]);
                 else break ; 
-            }
-            
+            }        
         }
         else if(strcmp(argval[0],"mv")==0 && !isBackground){
             mvFunction(argval[1], argval[2]);
@@ -88,7 +87,8 @@ int main (){
             getPath(cwd,1);
         }
         else if(strcmp(argval[0],"rm")==0 && !isBackground){
-            rmFunction(argval[1]);
+            if(strcmp(argval[1], "-r") ==0) rmFunction(argval[2],argval[1]);
+            else rmFunction(argval[1],"");
         }
         else if(strcmp(argval[0],"chmod")==0 && !isBackground){
             for(int i= 2; i<ARGMAX; i++){
@@ -222,7 +222,7 @@ void lsFunction(char* folderName){
     else n = scandir(folderName, &items, 0, alphasort);
 
     if (n >= 0){
-        printf("%s--- Total %d objects in this directory\n",BLUE,n-2);
+        printf("%s Total %d objects in this directory\n",BLUE,n-2);
         
         for(i = 2; i < n; i++ ){
             printContent(items[i]);
@@ -231,7 +231,7 @@ void lsFunction(char* folderName){
         printf("\n");
     }
     else{
-        perror ("--- Error in ls ");
+        perror ("Error in ls ");
     }
 }
 
@@ -399,12 +399,60 @@ void mkdirFunction(char* folderName){
     else perror(" Error: Cannot make directory ");
 }
 
-void rmFunction(char* folderName){
-    int answer = rmdir(folderName);
-    if(answer !=-1) printf(" successfully deleted directory %s %s %s \n", BLUE,folderName ,WHITE);
-    else perror(" Error: Cannot delete directory ");
-}
+// some parts referenced from 
+// https://stackoverflow.com/questions/2256945/removing-a-non-empty-directory-programmatically-in-c-or-c/2256974#2256974
 
+int rmFunction(char* fileFolderName,char* flag){
+   
+    if(strcmp(flag, "-r") ==0) {
+        size_t pathLength = strlen(fileFolderName);
+
+        DIR *folder = opendir(fileFolderName); 
+
+        int result = -1;
+
+        if (folder) {
+            struct dirent *items; // defining items array, of type directory entry, for removal of files inside directory 
+
+            result = 0;
+            while (!result && (items=readdir(folder))) {
+                
+                char *buf;
+                size_t len;
+                int itemResult = -1;
+                
+                // Skipping "."  and "..",as we do not want to recurse on those directories
+                if (!strcmp(items->d_name, ".") || !strcmp(items->d_name, ".."))
+                    continue;
+
+                len = pathLength + strlen(items->d_name) + 2; // +2 if a folder, and then there will be "."  and ".."
+                buf = malloc(len); // assign memory to buf variable
+
+                struct stat statbuf;  // defining stat struct variable to store the file properties
+                // just a fancy way to copy the path fileFolderName/files to buf, with maximum number of characters argument
+                snprintf(buf, len, "%s/%s", fileFolderName, items->d_name);
+                
+                if (items->d_type != DT_DIR) itemResult = remove(buf); // If not a directory, remove the file
+                else itemResult = rmFunction(buf,flag);  // If the item is a directory recurse and remove the contents. 
+                
+                free(buf); // freeing the assigned memory 
+            
+                result = itemResult; 
+            }
+            closedir(folder);
+        }
+        if (!result) result = rmdir(fileFolderName);
+
+        if(result !=-1) printf(" successfully deleted directory %s %s %s \n", BLUE,fileFolderName ,WHITE);
+        else perror(" Error: Cannot delete directory ");
+        return result; 
+    }
+    else{
+        if(remove(fileFolderName) ==0) printf(" successfully deleted directory %s %s %s \n", BLUE,fileFolderName ,WHITE);
+        else perror(" Error: Cannot delete file/directory ");    
+    }
+    
+}
 
 
 void inbuiltFunction(){
